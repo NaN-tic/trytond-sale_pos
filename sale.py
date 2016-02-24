3ce7bba9070ed2345de7a725e04a516d8bd11bcc
resteve <resteve@zikzakmedia.com>
2016-02-24 17:22:54 +0100

    Move prices with tax to sale_w_tax module

--- sale.py
+++ sale.py
@@ -226,27 +226,6 @@ class Sale:
 
 class SaleLine:
     __name__ = 'sale.line'
-    unit_price_w_tax = fields.Function(fields.Numeric('Unit Price with Tax',
-            digits=(16, Eval('_parent_sale', {}).get('currency_digits',
-                    Eval('currency_digits', 2))),
-            states={
-                'invisible': Eval('type') != 'line',
-                },
-            depends=['type', 'currency_digits']), 'get_price_with_tax')
-    amount_w_tax = fields.Function(fields.Numeric('Amount with Tax',
-            digits=(16, Eval('_parent_sale', {}).get('currency_digits',
-                    Eval('currency_digits', 2))),
-            states={
-                'invisible': ~Eval('type').in_(['line', 'subtotal']),
-                },
-            depends=['type', 'currency_digits']), 'get_price_with_tax')
-    currency_digits = fields.Function(fields.Integer('Currency Digits'),
-        'on_change_with_currency_digits')
-    currency = fields.Many2One('currency.currency', 'Currency',
-        states={
-            'required': ~Eval('sale'),
-            },
-        depends=['sale'])
 
     @classmethod
     def __setup__(cls):
@@ -263,27 +242,6 @@ class SaleLine:
         if Transaction().context.get('sale'):
             return Transaction().context.get('sale')
         return None
-
-    @staticmethod
-    def default_currency_digits():
-        Company = Pool().get('company.company')
-        if Transaction().context.get('company'):
-            company = Company(Transaction().context['company'])
-            return company.currency.digits
-        return 2
-
-    @staticmethod
-    def default_currency():
-        Company = Pool().get('company.company')
-        if Transaction().context.get('company'):
-            company = Company(Transaction().context['company'])
-            return company.currency.id
-
-    @fields.depends('currency')
-    def on_change_with_currency_digits(self, name=None):
-        if self.currency:
-            return self.currency.digits
-        return 2
 
     @fields.depends('sale')
     def on_change_product(self):
@@ -310,74 +268,6 @@ class SaleLine:
         if not self.sale:
             self.sale = Transaction().context.get('sale')
         return super(SaleLine, self).on_change_with_amount()
-
-    @classmethod
-    def get_price_with_tax(cls, lines, names):
-        pool = Pool()
-        Tax = pool.get('account.tax')
-        amount_w_tax = {}
-        unit_price_w_tax = {}
-
-        def compute_amount_with_tax(line):
-            tax_list = Tax.compute(line.taxes,
-                line.unit_price or Decimal('0.0'),
-                line.quantity or 0.0)
-            tax_amount = sum([t['amount'] for t in tax_list], Decimal('0.0'))
-            amount = line.amount
-            return (line.amount if amount else _ZERO) + tax_amount
-
-        for line in lines:
-            amount = Decimal('0.0')
-            unit_price = Decimal('0.0')
-            currency = (line.sale.currency if line.sale else line.currency)
-
-            if line.type == 'line':
-                if line.quantity:
-                    amount = compute_amount_with_tax(line)
-                    unit_price = amount / Decimal(str(line.quantity))
-
-            # Only compute subtotals if the two fields are provided to speed up
-            elif line.type == 'subtotal' and len(names) == 2:
-                for line2 in line.sale.lines:
-                    if line2.type == 'line':
-                        amount2 = compute_amount_with_tax(line2)
-                        if currency:
-                            amount2 = currency.round(amount2)
-                        amount += amount2
-                    elif line2.type == 'subtotal':
-                        if line == line2:
-                            break
-                        amount = Decimal('0.0')
-
-            if currency:
-                amount = currency.round(amount)
-            amount_w_tax[line.id] = amount
-            unit_price_w_tax[line.id] = unit_price
-
-        result = {
-            'amount_w_tax': amount_w_tax,
-            'unit_price_w_tax': unit_price_w_tax,
-            }
-        for key in result.keys():
-            if key not in names:
-                del result[key]
-        return result
-
-    @fields.depends('type', 'unit_price', 'quantity', 'taxes', 'sale',
-        '_parent_sale.currency', 'currency', 'product', 'amount')
-    def on_change_with_unit_price_w_tax(self, name=None):
-        if not self.sale:
-            self.sale = Transaction().context.get('sale')
-        return SaleLine.get_price_with_tax([self],
-            ['unit_price_w_tax'])['unit_price_w_tax'][self.id]
-
-    @fields.depends('type', 'unit_price', 'quantity', 'taxes', 'sale',
-        '_parent_sale.currency', 'currency', 'product', 'amount')
-    def on_change_with_amount_w_tax(self, name=None):
-        if not self.sale:
-            self.sale = Transaction().context.get('sale')
-        return SaleLine.get_price_with_tax([self],
-            ['amount_w_tax'])['amount_w_tax'][self.id]
 
     def get_from_location(self, name):
         res = super(SaleLine, self).get_from_location(name)
