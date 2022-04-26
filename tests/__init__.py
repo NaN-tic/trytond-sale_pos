47ac14fd51173a3d0fcc09f998aa1799f602b372
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-26 13:26:08 +0200

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++
@@ -1,8 +0,0 @@
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
-try:
-    from trytond.modules.sale_pos.tests.test_sale_pos import suite
-except ImportError:
-    from .test_sale_pos import suite
-
-__all__ = ['suite']
