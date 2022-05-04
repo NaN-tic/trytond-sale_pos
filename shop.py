fc1be3daaa52c07804399dfb89efac1859d61022
Jared Esparza <jared.esparza@nan-tic.com>
2022-05-04 11:11:24 +0200

    Add company to context

--- shop.py
+++ shop.py
@@ -3,13 +3,17 @@
 #the full copyright notices and license terms.
 from trytond.model import fields
 from trytond.pool import PoolMeta
+from trytond.pyson import Eval
 
 __all__ = ['SaleShop']
 
 
 class SaleShop(metaclass=PoolMeta):
     __name__ = 'sale.shop'
-    party = fields.Many2One('party.party', "Default Party")
+    party = fields.Many2One('party.party', "Default Party",
+        context={
+            'company': Eval('company'),
+        }, depends=['company'])
     self_pick_up = fields.Boolean('Default Self Pick Up',
         help='The goods are picked up by the customer before the sale, so no '
         'shipment is created.')
